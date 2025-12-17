aa0e17ea7660530a56b479b652d166bce21cb94f
Aqu1ver1 <maksym.hopaitsa@student.tuke.sk>
2025-12-17 12:21:56 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/finance-monitoring/sw.js', { scope: '/finance-monitoring/' })})}
